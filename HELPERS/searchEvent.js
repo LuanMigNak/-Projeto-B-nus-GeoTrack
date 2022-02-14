2db7fa36e89c5c85d84b1a20eceebad41863f0d9
viniciusyamate <viniciusyamate5@gmail.com>
2022-02-14 17:16:35 +0000

    conserta searchEvent

--- HELPERS/searchEvent.js
+++ HELPERS/searchEvent.js
@@ -1,12 +1,17 @@
 window.onload = () => {
   let searchButton = document.querySelector('.button');
   
+  const getIpData = async (ip) => {
+    const url = `https://api.techniknews.net/ipgeo/${ip}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+
   searchButton.addEventListener('click', () => {
-    console.log('teste')
+    let ipInput = document.querySelector('#IpInput')
+    return getIpData(ipInput.value);
   });
-}
-
-
-if (typeof module !== 'undefined') {
-  module.exports = searchButton;
-}
+};
